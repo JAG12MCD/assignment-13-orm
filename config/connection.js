ec0e40cbe5627cfd35a91a1a5f438280be2ab19b
JAG12 <garber.jacob@outlook.com>
2023-08-18 14:47:53 -0400

    assignment complete

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const Sequelize = require('sequelize');
+
+const sequelize = process.env.JAWSDB_URL
+	? new Sequelize(process.env.JAWSDB_URL)
+	: new Sequelize(
+			process.env.DB_NAME,
+			process.env.DB_USER,
+			process.env.DB_PASSWORD,
+			{
+				host: 'localhost',
+				dialect: 'mysql',
+				dialectOptions: {
+					decimalNumbers: true,
+				},
+			}
+	  );
+
+sequelize.sync().then(() => {
+	console.log('Database & tables created!');
+});
+
+module.exports = sequelize;
